5de20fa8bf14b39903db273e481017229f9169a1
tbeka <126447254+tbeka@users.noreply.github.com>
2024-01-23 13:27:19 +0100

    Update db.py

--- db.py
+++ db.py
@@ -20,3 +20,17 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
